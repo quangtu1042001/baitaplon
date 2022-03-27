626cf626aac5026f90b392a12ae17ac10475f262
satthudatinh <quangtu104@gmail.com>
2022-03-27 21:39:16 +0700

    giao dien trang chu

---
+++ baitaplonDidong/app/src/main/java/com/example/baitaplondidong/adapter/TruyenTranhAdapter.java
@@ -0,0 +1,49 @@
+package com.example.baitaplondidong.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.example.baitaplondidong.R;
+import com.example.baitaplondidong.object.TruyenTranh;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TruyenTranhAdapter extends ArrayAdapter<TruyenTranh> {
+    private Context ct;
+    private ArrayList<TruyenTranh> arr;
+    public TruyenTranhAdapter(@NonNull Context context, int resource, @NonNull List<TruyenTranh> objects) {
+        super(context, resource, objects);
+        this.ct = context;
+        this.arr = new ArrayList<>(objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView== null){
+            LayoutInflater inflater =(LayoutInflater)ct.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView =inflater.inflate(R.layout.item_truyen,null);
+        }
+        if(arr.size()>0){
+            TruyenTranh truyenTranh = this.arr.get(position);
+            TextView tenTenTruyen = convertView.findViewById(R.id.txvTenTruyen);
+            TextView tenTenChap = convertView.findViewById(R.id.txvTenChap);
+            ImageView imgAnhtruyen = convertView.findViewById(R.id.imgAnhTruyen1);
+
+            tenTenTruyen.setText(truyenTranh.getTenTruyen());
+            tenTenChap.setText(truyenTranh.getTenChap());
+            Glide.with(this.ct).load(truyenTranh.getLinkAnh()).into(imgAnhtruyen);
+        }
+        return convertView;
+    }
+}
