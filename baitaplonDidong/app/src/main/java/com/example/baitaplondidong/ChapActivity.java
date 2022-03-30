d962dc1e68f12f30e13e725180e08381a274d415
tranvantuann021201 <90585817+tranvantuann021201@users.noreply.github.com>
2022-03-30 21:21:27 +0700

    hiện danh sách chapter

--- baitaplonDidong/app/src/main/java/com/example/baitaplondidong/ChapActivity.java
+++ baitaplonDidong/app/src/main/java/com/example/baitaplondidong/ChapActivity.java
@@ -3,16 +3,25 @@ package com.example.baitaplondidong;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.baitaplondidong.adapter.ChapTruyenAdapter;
+import com.example.baitaplondidong.object.ChapTruyen;
 import com.example.baitaplondidong.object.TruyenTranh;
+
+import java.util.ArrayList;
 
 public class ChapActivity extends AppCompatActivity {
 TextView txvTenTruyens;
 ImageView imgAnhTruyens;
 TruyenTranh truyenTranh;
+ListView lsvDanhSachChap;
+ArrayList<ChapTruyen> arrChap;
+ChapTruyenAdapter chapTruyenAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +34,22 @@ TruyenTranh truyenTranh;
     private void init(){
         Bundle b = getIntent().getBundleExtra("data");
         truyenTranh = (TruyenTranh) b.getSerializable("truyen");
+        //tạo dữ liệu ảo
+        arrChap = new ArrayList<>();
+        for (int i = 1; i<20; i++) {
+            arrChap.add(new ChapTruyen("Chapter" + i , "Ngày đăng: 03-30-2022"));
+        }
+        chapTruyenAdapter = new ChapTruyenAdapter(this, 0, arrChap);
     };
     private void anhXa(){
         txvTenTruyens = findViewById(R.id.txvTenTruyens);
         imgAnhTruyens = findViewById(R.id.imgAnhTruyens);
+        lsvDanhSachChap = findViewById(R.id.lsvDanhSachChap);
     };
     private void setUp(){
         txvTenTruyens.setText(truyenTranh.getTenTruyen());
         Glide.with(this).load(truyenTranh.getLinkAnh()).into(imgAnhTruyens);
+        lsvDanhSachChap.setAdapter(chapTruyenAdapter);
     };
     private void setClick(){};
 }
---
+++ baitaplonDidong/app/src/main/java/com/example/baitaplondidong/object/ChapTruyen.java
@@ -0,0 +1,27 @@
+package com.example.baitaplondidong.object;
+
+public class ChapTruyen {
+    private String tenChap, ngayDang;
+    public ChapTruyen(){}
+
+    public ChapTruyen(String tenChap, String ngayDang) {
+        this.tenChap = tenChap;
+        this.ngayDang = ngayDang;
+    }
+
+    public String getTenChap() {
+        return tenChap;
+    }
+
+    public void setTenChap(String tenChap) {
+        this.tenChap = tenChap;
+    }
+
+    public String getNgayDang() {
+        return ngayDang;
+    }
+
+    public void setNgayDang(String ngayDang) {
+        this.ngayDang = ngayDang;
+    }
+}
