9528ca3ad8e613bd188db326b944f9e3ffdcc094
satthudatinh <quangtu104@gmail.com>
2022-04-06 15:29:33 +0700

    menu

--- baitaplonDidong/app/src/main/java/com/example/baitaplondidong/MainActivity.java
+++ baitaplonDidong/app/src/main/java/com/example/baitaplondidong/MainActivity.java
@@ -1,26 +1,38 @@
 package com.example.baitaplondidong;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.GridView;
 import android.widget.Toast;
 
+
 import com.example.baitaplondidong.adapter.TruyenTranhAdapter;
 import com.example.baitaplondidong.api.ApiLaytruyen;
+//import com.example.baitaplondidong.fragment.FavoriteFragmet;
+import com.example.baitaplondidong.fragment.FavoriteFragmet;
+import com.example.baitaplondidong.fragment.HomeFragment;
 import com.example.baitaplondidong.interfaces.LayTruyenVe;
 import com.example.baitaplondidong.object.TruyenTranh;
+import com.google.android.material.navigation.NavigationView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,7 +40,12 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements LayTruyenVe {
+public class MainActivity extends AppCompatActivity implements LayTruyenVe , NavigationView.OnNavigationItemSelectedListener{
+    private static final  int FRAGMENT_HOME = 0;
+    private static final  int FRAGMENT_FAVORITE = 1;
+
+    private int mCurrentFragment = FRAGMENT_HOME;
+
     GridView gdvDSTruyen;
     TruyenTranhAdapter adapter;
     ArrayList<TruyenTranh> truyenTranhArrayList;
@@ -50,13 +67,22 @@ public class MainActivity extends AppCompatActivity implements LayTruyenVe {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayour, toolbar ,R.string.nav_open, R.string.nav_close);
         mDrawerLayour.addDrawerListener(toggle);
         toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        replaceFragment(new HomeFragment());
+        navigationView.getMenu().findItem(R.id.nav_home).setChecked(true);
     }
+
+
     private void init(){
         truyenTranhArrayList = new ArrayList<>();
 
         adapter = new TruyenTranhAdapter(this,0,truyenTranhArrayList);
     }
-    private void anhXa(){ gdvDSTruyen = findViewById(R.id.gdvDSTruyen);
+    private void anhXa(){
+        gdvDSTruyen = findViewById(R.id.gdvDSTruyen);
         edtTimKiem = findViewById(R.id.edtTimKiem);
     }
     private void setUp(){gdvDSTruyen.setAdapter(adapter) ;}
@@ -72,6 +98,7 @@ public class MainActivity extends AppCompatActivity implements LayTruyenVe {
                 startActivity(intent);
             }
         });
+
         edtTimKiem.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -121,5 +148,41 @@ public class MainActivity extends AppCompatActivity implements LayTruyenVe {
     public void update(View view) {
         new ApiLaytruyen(this).execute();
 
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_home){
+            if(mCurrentFragment != FRAGMENT_HOME){
+                replaceFragment(new HomeFragment());
+                mCurrentFragment = FRAGMENT_HOME;
+            }
+
+        }else if (id == R.id.favorite_nav) {
+            if(mCurrentFragment != FRAGMENT_FAVORITE){
+                replaceFragment(new FavoriteFragmet());
+                mCurrentFragment = FRAGMENT_FAVORITE;
+            }
+        }
+
+        mDrawerLayour.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayour.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayour.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, fragment);
+        transaction.commit();
     }
 }
---
+++ baitaplonDidong/app/src/main/java/com/example/baitaplondidong/object/setMenu.java
@@ -0,0 +1,2 @@
+package com.example.baitaplondidong.object;public class setMenu {
+}
